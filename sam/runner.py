3bc062b4e1ce801a361262d4bbd8db45181e5150
Kuba Clark <jakub.clark@protonmail.com>
2018-10-08 05:52:12 +0200

    followup weather location for given time is possible

--- sam/runner.py
+++ sam/runner.py
@@ -5,7 +5,7 @@ from string import ascii_uppercase, digits
 from flask import Flask
 from flask.json import jsonify
 
-from .exceptions import SamException
+from .exceptions import SamError
 from .routes import setup_routes
 
 
@@ -14,7 +14,7 @@ def create_app():
     app_.secret_key = os.environ.get('SECRET_KEY', ''.join(choices(ascii_uppercase + digits, k=12)))
     setup_routes(app_)
 
-    @app_.errorhandler(SamException)
+    @app_.errorhandler(SamError)
     def handle_invalid_data_format(error):
         response = jsonify(error.to_dict())
         response.status_code = error.status_code
